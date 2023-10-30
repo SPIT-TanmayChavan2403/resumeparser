979592d521dce17dc522bea7b3b33d0efebc5dff
TanmayChavan-2403 <tanmaychavan1306@gmail.com>
2023-10-30 23:05:40 +0530

    Completed project

--- src/components/Scorer/Scorer.jsx
+++ src/components/Scorer/Scorer.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
-
+import { useState, useRef } from "react";
+import s from './Scorer.module.css'
+import { json } from "react-router-dom";
 
 export default function Scorer(props) {
     const [files, setFiles] = useState([])
+    const [result, setResult] = useState([])
+    const [jd, setJD] = useState("")
+    const inputRef = useRef(null);
 
-    const takeInput = (e) => {
+    const takeFiles = (e) => {
         const data = new FormData();
-        setFiles(e.target.files);
-        console.log(e.target.files)
+        setFiles(inputRef.current.files);
     }
 
     const upload = (e) => {
@@ -15,20 +18,57 @@ export default function Scorer(props) {
         for (let i = 0; i < files.length; i++) {
             data.append(`image ${i}`, files[i]);
         }
-        fetch("http://localhost:5000/upload", {
+
+        fetch("https://20.243.20.210:5000/upload", {
             method: "POST",
-            body: data
+            body: data,
+            // body: JSON.stringify({files: data, jobDescription: jd}),
         }).then(res => res.json()).then(data => {
-            console.log(data);
+            let tempres = [];
+            for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                    tempres.push(`Score of ${key} is: ${data[key]}`);
+                }
+            }
+            setResult(tempres);
         }).catch(err => {
             console.log(err);
         })
     }
 
+    const handleJD = (e) => {
+        setJD(e.target.value)
+    }
+
     return (
-        <div className="scorer">
-            <input type="file" multiple onChange={takeInput} />
-            <button onClick={upload}>Upload</button>
+        <div id={s.wrapper}>
+            <div id={s.top_section}>
+                <div id={s.jd_container}>
+                    <h2>Enter Job Description</h2>
+                    <textarea value={jd} name="" id="" cols="30" rows="5" onChange={handleJD} ></textarea>
+                </div>
+                <div id={s.resume_container}>
+                    <h2>Upload Resume</h2>
+                    
+                    <button onClick={() => {inputRef.current.click()}}>Select Files</button>
+                    <input onChange={takeFiles} ref={inputRef} type="file" multiple  id={s.inputField} />
+                    <button onClick={upload}>Upload</button>
+                    <div id={s.resume_list}>
+                        <ul>
+                            {Array.from(files).map(file => {
+                                return <li key={file.name}>{file.name}</li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id={s.bottom_section}>
+                <ul>
+                        {Array.from(result).map(file => {
+                            return <li key={file}>{file}</li>
+                        })}
+                </ul>
+            </div>
         </div>
     );
 }
